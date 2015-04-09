9dac70f34a782ff069cc4146dd792b0745fae539
Justin Freeman <freeman.justin@gmail.com>
2015-04-09 22:11:45 +1000

    added code to flip the image data in y before calling the stb_write_png() function. screenshots are now correct!

--- src/save_image.cpp
+++ src/save_image.cpp
@@ -74,11 +74,14 @@ void WindowDump_PPM(void)
 
 void WindowDump_PNG(void)
 {
+    int i,j;
     FILE *fptr = NULL;
     static int counter = 0; /* This supports animation sequences */
     char fname[64];
     //unsigned char *image;
     char	*image;
+    char    *flipImage;
+    int     index;
     GLint width;
     GLint height;
     GLint viewport[4];
@@ -93,6 +96,12 @@ void WindowDump_PNG(void)
         return;
     }
     
+    // Allocate our buffer for the image
+    if ((flipImage = (char *) malloc(height*width*3*sizeof(char)) ) == NULL) {
+        fprintf(stderr,"write_png: Failed to allocate memory for image \n");
+        return;
+    }
+    
     glPixelStorei(GL_PACK_ALIGNMENT,1);
     
     sprintf(fname,"window%04d.png",counter);
@@ -101,10 +110,21 @@ void WindowDump_PNG(void)
     glReadBuffer(GL_BACK_LEFT);
     glReadPixels(0,0,width,height,GL_RGB,GL_UNSIGNED_BYTE,image);
     
-    stbi_write_png(fname, width, height, 3, (const void *)image, 0);
+    // flip the y axis of the image data before writing
+    index = 0;
+    for (j=height-1;j>=0;j--) {
+        for (i=0;i<width;i++) {
+            flipImage[index] = image[3*j*width+3*i+0]; index++;
+            flipImage[index] = image[3*j*width+3*i+1]; index++;
+            flipImage[index] = image[3*j*width+3*i+2]; index++;
+        }
+    }
+    
+    stbi_write_png(fname, width, height, 3, (const void *)flipImage, 0);
     
     counter++;
     free(image);
+    free(flipImage);
     fclose(fptr);
     //printf("Saved png image\n");
 }
